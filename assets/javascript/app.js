198523bb94b12c05f703e39f2585a9127f1e16ac
mVang <meng.vang92@yahoo.com>
2018-06-06 15:23:54 -0500

    More things

---
+++ assets/javascript/app.js
@@ -0,0 +1,32 @@
+// Make an object of questions
+
+$(document).ready(function() {
+    // Questions object
+    var questions = {
+        firstQuestion: {
+            question: "Suppose there is an alien civilization 66 million light-years away from Earth. What would they see if they looked at the Earth right now?",
+            answer: "a"
+        },
+        secondQuestion: {
+            question: "Suppose there is an alien civilization 66 million light-years away from Earth. What would they see if they looked at the Earth right now?",
+            answer: "b"
+        },
+        thirdQuestion: {
+            question: "Suppose there is an alien civilization 66 million light-years away from Earth. What would they see if they looked at the Earth right now?",
+            answer: "c"
+        },
+    }
+    // Test questions
+    // console.log(questions.thirdQuestion.answer)
+
+    
+
+
+    function start(){
+
+    }
+
+    // Greet the player
+    $(".welcome").append('<h2 style="text-align:center">Welcome! Press start to play!</h2>')
+
+});

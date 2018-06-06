3d80380c12533fc87c4439529ac6e441c9e2dfd1
mVang <meng.vang92@yahoo.com>
2018-06-06 17:11:09 -0500

    More things

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,47 @@
 // Make an object of questions
 
-$(document).ready(function() {
-    // Global variables
-    var timer = "";
+$(document).ready(function(){
     // Questions object
     var questions = [{
             ask: "Suppose there is an alien civilization 66 million light-years away from Earth. What would they see if they looked at the Earth right now?",
             answer: "a"
         },{
             ask: "Question 2?",
-            answer: "5"
+            answer: "b"
         },{
             ask: "Question 3?",
             answer: "c"
         },{
             ask: "Question 4?",
-            answer: "c"
+            answer: "d"
     }];
 
     // Starts the game
     function start(){
         // Removes welcome text and buttons before inserting questions and answers
-        $(".welcome").html('<h2 style="text-align:center">// Timer</h2>');
+        
         $(".startButton").prev().addBack().remove();
 
         // Loop through the questions array and put the questions into HTML
         for (var i = 0; i < questions.length; i++){
             $(".questions").append('<h4>'+ questions[i].ask + '</h4><br>');
         }
-
+        
+        // var timer = duration, minutes, seconds;
+        // setInterval(function () {
+        //     minutes = parseInt(timer / 60, 10)
+        //     seconds = parseInt(timer % 60, 10);
+    
+        //     minutes = minutes < 10 ? "0" + minutes : minutes;
+        //     seconds = seconds < 10 ? "0" + seconds : seconds;
+    
+        //     display.textContent = minutes + ":" + seconds;
+    
+        //     if (--timer < 0) {
+        //         timer = duration;
+        //     }
+        // }, 1000);
+        
     }
 
     // Greet the player

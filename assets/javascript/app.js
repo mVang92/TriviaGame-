75f512fadf3a967b77b6c299af7f2b4e296ef337
mVang <meng.vang92@yahoo.com>
2018-06-20 12:17:21 -0500

    Added comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,173 +1,155 @@
-$(document).ready(function(){
-    $(".welcome").append('<h2 style="text-align:center">Welcome! Press start to play!</h2>');
-    $("#startButton").append('<button type="button" class="btn btn-danger btn-lg">Start</button>');
-    $("#submit").hide();
+// Global Variables Start
+var currentQuestion; 
+var correctAnswer; 
+var incorrectAnswer; 
+var unAnswered; 
+var seconds; 
+var time; 
+var answered; 
+var userSelect;
+var randomTheme;
+// Message object to display status to user
+var messages = {
+	correct: "<h2>Correct!</h2>",
+	incorrect: "<h3>Incorrect.</h3>",
+	timeUp: "<h2>Time is up!</h2>",
+	finished: "<h2>Your score:</h2>"
+}
+// End Global Variables
 
-    var count = 45;
-    var timerOn = false;
-    var currentQuestion = 0;
-    var correct = 0;
-    var wrong = 0;
-    var name;
-    
-    // Questions
-    var questions = [
-        "Suppose there is an alien civilization 66 million light-years away from Earth. What would they see if they looked at the Earth right now?",
-        "What is the closest star system to our solar system?",
-        "The largest moon in our solar system belongs to which one of these planets?",
-    ];
+// Hides the reset button at starup
+$("#reset").hide();
 
-    // Choices for questions
-    var choices = [
-        [
-        "The Dinosaurs",
-        "The Roman Empire",
-        "An ocean planet",
-        "The Incredibles, of course"
-        ],
-        [
-        "Betelgeuse",
-        "Sirius",
-        "Alpha Centauri",
-        "The Moon"
-        ],
-        [
-        "Gliese 581",
-        "Saturn",
-        "Jupiter",
-        "Neptune"
-        ],
-    ];
-    
-    // References to correct answers in choices array
-    var correctAnswer = [
-        choices[0][0],
-        choices[1][2],
-        choices[2][2],
-    ];
-    
-    // Start button functions
-    $("#startButton").click(function(){
-        $(".welcome").html('<h2 style="text-align:center">Question ' + parseInt(currentQuestion  + 1) + '</h2>')
-        $("#submit").show();
-        $("#quiz").show();
-        $("#correctResults").hide();
-        $("#wrongResults").hide();
-        $(this).hide();
-        timerOn = false;
-        currentQuestion = 0;
-        correct = 0;
-        wrong = 0;
-        count = 45;
-        
-        // Running method to run game
-        function running(){
-            var choiceToQuestion = 0;
-            var i = 0;
-            var j = 0;
-
-            // Append the questions from the questions array
-            for (i = 0; i < questions.length; i++){
-                // Reset value to 0 
-                value = 0;
-                $("#quiz").append('<h4>' + questions[i] + '</h4><br>');
-
-                // For each question, append the elements in the choices array
-                for (j = 0; j <= choices.length; j++){
-                    // Changes the value 1 to 4 for each question
-                    value++;
-                    $("#quiz").append('<div><input type="radio" value="'+ choices[i][j] +'" name="option' + choiceToQuestion + '">' + choices[i][j] + '</div><br>');
-                }
-                // Increment name options after each question
-                choiceToQuestion++;
-            }
-            
-            // Timer function starts timer
-            function timer() {
-                $("#timer").html('<h2 style="text-align:center">' + count + '</h2>');
-
-                if (count === -1){
-                    // Show Time is up!
-                    $("#timer").html('<h2 style="text-align:center">Time is up!</h2>');
-                    count = 0;
-                    stopCount();
-                    $("#submit").hide();
-                    $("#quiz").hide();
-                    // $("#startButton").show();
-
-                    } else {
-                        count -= 1;
-                        time = setTimeout(timer, 1000);
-                    }
-                }
-                        
-                function startCount() {
-                    if (!timerOn) {
-                        timerOn = true;
-                        timer();
-                    }
-                }
-
-                function stopCount() {
-                    timerOn = false;
-                    count = 0;
-                }
-
-                // Checks for clicks on all radio buttons
-                $('input:radio').click(function(){
-                    // name variable will obtain the name attribute for the radio button clicked
-                    name = ($(this).attr("name"));
-                    optionGroup = parseInt(name.replace("option", ""));
-                })
-
-                // Submit button function
-                $("#submit").click(function(){
-                    $("#correctResults").show();
-                    $("#wrongResults").show();
-                    // $("#startButton").show();
-                    $("#quiz").hide();
-                    $("#timer").hide();
-                    $(this).hide();
-                    count = 0;
-                    timer = false;
-                    
-                    // Loop through the questions 
-                    for (var n = 0; n < questions.length; n++){
-                        // Give each questions a unique option number
-                        var option = $("[name='option" + n + "']");
-                        // console.log(option)
-
-                        // Nested for loop to go through each answer choices for each question
-                        for(var g = 0; g < option.length; g++){
-                            // Checks to see if all radio buttons are checked
-                            if ($(option[g]).is(":checked") && $(option[g]).val() === correctAnswer[n]){
-                                correct++;
-                                // console.log(correct)
-                                // Hide this (submit) button
-                                $(this).hide();
-                                $("#correctResults").html('<h3>Questions Correct: ' + correct);
-                            
-                            // Else if user choice is not equal to th e correct answer...
-                            } else if ($(option[g]).is(":checked") && $(option[g]).val() != correctAnswer[n]){
-                                wrong++;
-                                $("#wrongResults").html('<h3>Questions Incorrect: ' + wrong);
-                                //-------------------------------------------------------
-                                
-                                // This is where I could put some code to check for
-                                // unchecked buttons and display them on the screen.
-
-                                // Some code to prevent the submit button from working
-                                // if there are unchecked radio buttons
-
-                                //-------------------------------------------------------
-
-                            }
-                        }
-                    }
-                })
-            // Begin timer
-            startCount();
-            }
-        running()
-    });
+// When clicked, hide the start button and run the game
+$("#startBtn").on("click", function(){
+	$(this).hide();
+	running();
 });
+
+// When clicked, hide the reset button and run the game
+$("#reset").on("click", function(){
+	$(this).hide();
+	running();
+});
+
+function running(){
+	// Initialize variables back to 0
+	currentQuestion = 0;
+	correctAnswer = 0;
+	incorrectAnswer = 0;
+	unAnswered = 0;
+	// Remove any records from HTML so the user cannot see them
+	$("#finalMessage").empty();
+	$("#correctAnswers").empty();
+	$("#incorrectAnswers").empty();
+	$("#unAnswered").empty();
+	// Call the next question
+	newQuestion();
+}
+
+function newQuestion(){
+	$("#message").empty();
+	$("#correctedAnswer").empty();
+	// Displays the current question number out of total questions
+	$("#currentQuestion").html("<h3>Question " + (currentQuestion + 1 ) + " of " + questions.length + "</h3>");
+	// Displays the current question
+	$(".question").html("<h2>" + questions[currentQuestion].question + "</h2>");
+	answered = true;
+	// Randomize the theme with a number 1, 2, or 3
+	randomTheme = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+	// New theme variable will contain what element apiTheme should reference
+	var theme = apiTheme[randomTheme];
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+		theme + "&api_key=whIW4NL8ItI77ZD3d2Yomtb0G40WFANS&limit=13";
+	// Ajax start
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).then(function(response) {
+		var results = response.data;
+		// Each theme will have 12 random search results
+		var random = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+		var image = $("<img src=" + results[random].images.fixed_height.url + ">");
+		$("#questionImg").html(image);
+	})
+
+	// Displays our answer choices in relation to each questions
+	for (var i = 0; i < 4; i++){
+		var choices = $("<div>");
+		choices.text(questions[currentQuestion].answerList[i]);
+		choices.attr({"data-index": i });
+		choices.addClass("thisChoice");
+		$(".answerList").append(choices);
+    }
+    
+	countdown();
+	$(".thisChoice").on("click", function(){
+		userSelect = $(this).data("index");
+		clearInterval(time);
+		showAnswer();
+	});
+}
+
+// Timer function
+function countdown(){
+	seconds = 20;
+	$("#timer").html("<h3>Time: " + seconds + "</h3>");
+	answered = true;
+	time = setInterval(showCountdown, 1000);
+}
+// This function is called every second to decrement seconds
+function showCountdown(){
+	seconds--;
+	$("#timer").html("<h3>Time: " + seconds + "</h3>");
+	// When seconds is less than 1, call the showAnswer function
+	if (seconds < 1){
+		clearInterval(time);
+		answered = false;
+		showAnswer();
+	}
+}
+
+function showAnswer(){
+	$("#currentQuestion").empty();
+	$(".thisChoice").empty();
+	$(".question").empty();
+
+    var rightAnswerText = questions[currentQuestion].answerList[questions[currentQuestion].answer];
+    var rightAnswerIndex = questions[currentQuestion].answer;
+    
+	if ((userSelect == rightAnswerIndex) && (answered == true)){
+		correctAnswer++;
+		$("#message").html(messages.correct);
+	} else if ((userSelect != rightAnswerIndex) && (answered == true)){
+		incorrectAnswer++;
+		$("#message").html(messages.incorrect);
+		$('#correctedAnswer').html("<h3>Correct answer: <br><h2>" + rightAnswerText + "</h2></h3>");
+	} else {
+		unAnswered++;
+		$("#message").html(messages.timeUp);
+		$("#correctedAnswer").html("<h3>Correct answer: <br><h2>" + rightAnswerText + "</h2></h3>");
+		answered = true;
+	}
+	// Between each question, wait 4 seconds before contining to the next question
+	if (currentQuestion == (questions.length - 1)){
+		setTimeout(scoreboard, 4000)
+	} else {
+		currentQuestion++;
+		setTimeout(newQuestion, 4000);
+	}	
+}
+
+// Displays the score to the user
+function scoreboard(){
+	$("#timer").empty();
+	$("#message").empty();
+	$("#correctedAnswer").empty();
+	$("#questionImg").empty();
+	$("#finalMessage").html(messages.finished);
+	$("#correctAnswers").html("<h4>Correct Answers: " + correctAnswer + "</h4>");
+	$("#incorrectAnswers").html("<h4>Incorrect Answers: " + incorrectAnswer + "</h4>");
+	$("#unAnswered").html("<h4>Unanswered: " + unAnswered + "</h4>");
+	// Display a reset button for the user
+	$("#reset").fadeIn();
+	$("#reset").html("<h2>Reset Game</h2>");
+}
